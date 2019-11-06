7daa84a9077402f5fcc3401fdfced7e2fef442d2
codezyx <codezyx@yahoo.com>
2019-11-06 16:11:34 +0800

    refactor

--- src/main/java/com/hguoli/print/PdfUtil.java
+++ src/main/java/com/hguoli/print/PdfUtil.java
@@ -1,39 +1,41 @@
 package com.hguoli.print;
 
-import com.itextpdf.io.font.FontConstants;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
-import com.itextpdf.layout.property.TextAlignment;
-import com.itextpdf.layout.property.VerticalAlignment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
-import java.io.FileNotFoundException;
-
-import static com.itextpdf.kernel.pdf.PdfName.BaseFont;
+import java.util.Arrays;
+import java.util.List;
 
 public enum PdfUtil {
 
     INSTANCE;
     private static final Logger LOGGER = LoggerFactory.getLogger(PdfUtil.class);
+    private static final int FIRST_INDEX = 0;
 
     public static void main(String[] args) {
-        PdfUtil.INSTANCE.createPdf("C:\\Users\\user\\Desktop\\test.pdf");
+        List<String[]> list = CSVUtil.INSTANCE.readAll("C:\\Users\\user\\Desktop\\histable.csv");
+        PdfUtil.INSTANCE.createPdf(list, Arrays.asList(0, 1, 2, 3, 4), "C:\\Users\\user\\Desktop\\test.pdf");
     }
 
-    public void createPdf(String pdfFile) {
+    public void createPdf(List<String[]> data, List<Integer> indexes, String pdfFile) {
         Document doc = null;
         try {
             PdfDocument pdfDocument = new PdfDocument(new PdfWriter(pdfFile));
             doc = new Document(pdfDocument, PageSize.A4);
-            doc.setFont(PdfFontFactory.createFont("STSong-Light", "UniGB-UCS2-H",true));
-            doc.add(createTable());
+            doc.setFont(PdfFontFactory.createFont("STSong-Light", "UniGB-UCS2-H", true));
+            doc.setFontSize(8);
+            Table table = createTable(data, indexes);
+            if (null != table) {
+                doc.add(table);
+            }
         } catch (Exception e) {
             LOGGER.error("Error to write pdf: " + pdfFile, e);
         } finally {
@@ -43,11 +45,48 @@ public enum PdfUtil {
         }
     }
 
-    private Table createTable() {
-        Table table = new Table(3);
-        table.addCell("1111");
-        table.addCell("中文");
-        table.addCell("333");
+    private Table createTable(List<String[]> data, List<Integer> indexes) {
+        if (data.isEmpty() || null == indexes || indexes.isEmpty()) {
+            return null;
+        }
+        Table table = new Table(indexes.size());
+
+        // 添加表头
+        String[] headers = data.get(FIRST_INDEX);
+        for (Integer index : indexes) {
+            Cell cell = new Cell();
+//            cell.setPaddingLeft(20);
+            cell.add(new Paragraph(headers[index]));
+            table.addCell(cell);
+        }
+        // 添加数据
+        for (int i = 1; i < data.size(); i++) {
+            String[] row = data.get(i);
+            if (isNotEmpty(row)) {
+                for (Integer index : indexes) {
+                    table.addCell(row[index]);
+                }
+            }
+        }
         return table;
+    }
+
+    /**
+     * 判断一行数据是否都是空
+     *
+     * @param row String[]
+     * @return boolean
+     */
+    private boolean isNotEmpty(String[] row) {
+        if (null == row || row.length == 0) {
+            return false;
+        } else {
+            for (String dat : row) {
+                if (null != dat && !"".equals(dat.trim())) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
